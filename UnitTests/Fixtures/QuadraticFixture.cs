fe81a7563e7e440e2165882727c567e431eccbbc
Jonas Lima <jonas1307@live.com>
2018-01-25 00:11:49 -0200

    Code coverage

---
+++ UnitTests/Fixtures/QuadraticFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests.Fixtures
+{
+    [TestFixture]
+    public class QuadraticFixture
+    {
+        [Test]
+        public void Test()
+        {
+            Solve.Quadratic(1, 10, 16);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                Solve.Quadratic(1, 1, 1);
+            });
+        }
+    }
+}
