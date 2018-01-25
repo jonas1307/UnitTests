0ad713a30f9c94949fad05458d9bef948a9afcf5
Jonas Lima <jonas1307@live.com>
2018-01-25 01:03:14 -0200

    Joining BankAccountFixture and DataDrivenTestsFixture

--- UnitTests/Fixtures/BankAccountFixture.cs
+++ UnitTests/Fixtures/BankAccountFixture.cs
@@ -6,12 +6,12 @@ namespace UnitTests.Fixtures
     [TestFixture]
     public class BankAccountFixture
     {
-        private BankAccount _bankAccount;
+        private BankAccount _ba;
 
         [SetUp]
         public void Setup()
         {
-            _bankAccount = new BankAccount(100);
+            _ba = new BankAccount(100);
         }
 
         [Test]
@@ -19,17 +19,32 @@ namespace UnitTests.Fixtures
         {
             // AAA: Arrange, Act and Assert
 
-            _bankAccount.Deposit(100);
+            _ba.Deposit(100);
 
-            Assert.That(_bankAccount.Balance, Is.EqualTo(200));
+            Assert.That(_ba.Balance, Is.EqualTo(200));
         }
 
         [Test]
         public void BankAccountShouldThrowOnNonPositiveAmount()
         {
-            var ex = Assert.Throws<ArgumentException>(() => _bankAccount.Deposit(-100));
+            var ex = Assert.Throws<ArgumentException>(() => _ba.Deposit(-100));
 
             StringAssert.StartsWith("Deposit amount should be positive", ex.Message);
+        }
+
+        [Test]
+        [TestCase(50, true, 50)]
+        [TestCase(100, true, 0)]
+        [TestCase(1000, false, 100)]
+        public void TestMultipleWithdrawScenarios(int amountToWithdraw, bool shouldSucceed, int expectedBalance)
+        {
+            var result = _ba.Withdraw(amountToWithdraw);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.EqualTo(shouldSucceed));
+                Assert.That(_ba.Balance, Is.EqualTo(expectedBalance));
+            });
         }
     }
 }
