8ffa6692eb5aac18ff722af6168d8640f8d8cdc7
Jonas Lima <jonas1307@live.com>
2018-01-24 23:23:04 -0200

    Creating first tests

--- UnitTests/BankAccountTests.cs
+++ UnitTests/BankAccountTests.cs
@@ -5,10 +5,22 @@ namespace UnitTests
     [TestFixture]
     public class BankAccountTests
     {
+        private BankAccount _bankAccount;
+
+        [SetUp]
+        public void Setup()
+        {
+            _bankAccount = new BankAccount(100);
+        }
+
         [Test]
         public void BankAccountShouldIncreaseOnPositiveDeposit()
         {
-            
+            // AAA: Arrange, Act and Assert
+
+            _bankAccount.Deposit(100);
+
+            Assert.That(_bankAccount.Balance, Is.EqualTo(200));
         }
     }
 }
